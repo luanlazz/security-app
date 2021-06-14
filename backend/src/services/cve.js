851d78450dd43a7dc09ec52cb0e3854027ac0c36
luanlazz <luanlazzari@hotmail.com>
2021-06-14 01:21:09 -0300

    implementation of cve search by id

--- backend/src/services/cve.js
+++ backend/src/services/cve.js
@@ -1,5 +1,33 @@
+const axios = require('axios')
+
 module.exports = {
   async getCveById(req, res) {
-    res.send('Test')
+    const idParam = req.params.id
+    const cod = idParam.replace(/(^CVE)(\ |\-)([0-9]{4})(\ |\-)([0-9]{4,})/gm, '$1-$3-$5')
+    if (!/^CVE-[0-9]{4}-[0-9]{4,}/gm.test(cod)) {
+      return res.status(404).send('Código inválido, por favor confira e tente novamente')
+    }
+    const url = `${process.env.URL_API_CIRCL}/${cod}`
+
+    axios.get(url)
+      .then(response => {
+        if (!response.data) {
+          res.status(204).send()
+        } else {
+          const { cwe, id, cvss, impact, msbulletin, references, summary } = response.data
+          res.status(200).json({
+            cwe,
+            id,
+            cvss,
+            impact,
+            msbulletin,
+            references,
+            summary
+          })
+        }
+      })
+      .catch(err => {
+        res.status(500).send('Ocorreu um erro na consulta, tente novamente mais tarde')
+      })
   }
 }
